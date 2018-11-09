775d1ebac9719b30b33f0e6e19d16a0955a9a5e2
Maria Yala <denise_myala@yahoo.com>
2018-11-09 08:08:03 -0500

    Got the points tracing to work. Added rainbow color animation

--- artfulprotest/js/sketch.js
+++ artfulprotest/js/sketch.js
@@ -23,30 +23,37 @@ var dumpCnt = 0;
 var blackoutFont;
 var goudyFont;
 var junctionFont;
+var oldNewsFont;
 var msg, msg1;
+var hue;
 
 function preload(){
      blackoutFont = loadFont('./fonts/Blackout Sunrise.ttf');
      goudyFont = loadFont('./fonts/OFLGoudyStM.otf');
      junctionFont = loadFont('./fonts/Junction-bold.otf');
+     oldNewsFont = loadFont('./fonts/OldNewspaperTypes.ttf');
 }
 
 /* Automatically fill the window */
 function setup(){
      canvas = createCanvas(windowWidth, windowHeight);
+     canvas.style('display','block');
      serial = new p5.SerialPort(); // Create a serial port object
      serial.open(serialPortName); // open the serial port
      serial.on('open', ardCon); // open the socket connection and execute the ardCon callback
      serial.on('data', dataReceived); // execute the dataReceived function when data is received
      frameRate(2);
+     hue = 0;
 }
 
 function draw(){
      if(isProtesting){
-          background(0); // Black
           // Check the sensor values and choose an animation
+          animation = 1;
           if(animation == 0){
                // Dump Trump
+               colorMode(RGB, 255);
+               background(0); // Black
                var women = "W o m e n";
                var d = "D ";
                var tr = "T r ";
@@ -55,6 +62,7 @@ function draw(){
                textAlign(CENTER,CENTER);
                textFont(blackoutFont);
                fill(255);
+               noStroke();
                text(women, 0, 0, width, height/2);
                if((dumpCnt % 2) == 0){
                     msg = d + ump;
@@ -66,15 +74,56 @@ function draw(){
                dumpCnt += 1;
           } else if(animation == 1){
                // Love is Love
+               colorMode(RGB, 255);
+               background(0); // Black
                var love = "L O V E";
+               var is = "I S"
+               var love2 = "L O V E";
+               textSize(150);
+               textAlign(CENTER,CENTER);
+               textFont(junctionFont);
+               noStroke();
+               fill(255,0,0);
+               stroke(255,0,0);
+               strokeWeight(7);
+               text(love,0,0,width,height/3);
 
+               textAlign(CENTER,CENTER);
+               fill(255);
+               noStroke();
+               textSize(50);
+               text(is,0,height/4,width,height/3);
+
+               var points = junctionFont.textToPoints(love2,1*width/4,5*height/6,200);
+               // console.log(points);
+               var c = 0;
+               console.log(points.length);
+               for(var i = 0; i < points.length; i++){
+                    var pt = points[i];
+                    colorMode(HSL, 360);
+                    stroke(hue,200,200);
+                    strokeWeight(9);
+                    point(pt.x,pt.y);
+                    if (hue > 360){
+                         hue = 0;
+                    } else {
+                         hue++;
+                    }
+               }
+               // Hue - https://medium.com/@kellylougheed/rainbow-paintbrush-in-p5-js-e452d5540b25
+               // Points - https://www.youtube.com/watch?v=4hA7G3gup-4
+
+          } else {
+               // Another animation here
           }
      } else {
           // Person isn't raising the protest sign, show watch screen
+          colorMode(RGB, 255);
           background(255);
-          textSize(82);
-          textFont(goudyFont);
+          textSize(78);
+          textFont(oldNewsFont);
           fill(0);
+          noStroke();
           textAlign(CENTER,TOP);
           msg = "...Climate change is real...Time's up...Families are seperated at the border...Toxic masculinity kills...Hate crimes are on the rise...Black lives are in danger...We need stricter gun control..."
           text(msg, 0, 20, width, height);
